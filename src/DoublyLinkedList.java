db69047b6813a5eeedad88381dbed286ec639918
shimanshu <shimanshusharma21july@gmail.com>
2019-03-06 15:28:55 +0530

    complete

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,24 +1,72 @@
+
+class Node{
+    int data;
+    Node next;
+    Node prev;
+}
+
 public class DoublyLinkedList {
+    int size = 0;
+    Node head = null;
+    Node tail = null;
 
-    public static class Node{
-        int data;
-        Node next;
-        Node prev;
 
+
+    void insert(Node node){
+        System.out.println("inserting node " + node.data + " at last" );
+        if(head == null){
+            head = node;
+        }
+        else {
+            Node temp = head;
+            while (temp.next != null){
+                temp = temp.next;
+            }
+            node.prev = temp;
+            temp.next = node;
+        }
     }
 
     public static void main(String[] args) {
         DoublyLinkedList linkedList = new DoublyLinkedList();
-        Node node = new Node();
-        node.data = 4;
-        node.next = null;
-        node.prev = null;
-        linkedList.insert(node);
+
+        for (int i = 0; i<5; i++ ){
+
+            Node node = new Node();
+            node.data = (i + 1) * 1000;
+            linkedList.insert(node);
+        }
+
+        linkedList.display();
+        linkedList.delete();
+        linkedList.display();
     }
 
-    void insert(Node node){
-        System.out.println("inserting node " + node.data + "at last" );
+    void delete (){
+        if(head == null){
+            System.out.println("empty list");
+        }
+        else{
+        Node temp = head;
+        while (temp.next != null){
+            temp = temp.next;
+            }
+            System.out.println("deleting node " + temp.data + " from last");
+            temp = temp.prev;
+            temp.next = null;
+        }
+
     }
 
+    void display(){
+        System.out.println("Doubly linkedList");
+        Node temp = head;
+        System.out.println("null <...>");
+        while(temp != null){
+            System.out.println(temp.data + "<...>");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
 
 }
